44c035d226f4b2a9b32a0e869a1f755bc8424ed4
Islem <islem@autoloc.com>
2026-05-17 17:01:45 +0100

    Hide number input spinners + disable mouse-wheel value changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Empêcher la molette de modifier les inputs number (source de bugs)
+document.addEventListener('wheel', (e) => {
+  if (document.activeElement?.type === 'number') {
+    document.activeElement.blur();
+  }
+}, { passive: true });
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
